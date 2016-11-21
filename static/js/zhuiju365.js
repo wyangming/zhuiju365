89b30f53ae48a4545e391c84870df827b76cd6ed
wyangming <479027247@qq.com>
2016-11-21 21:41:00 +0800

    Signed-off-by: wyangming <479027247@qq.com>

--- static/js/zhuiju365.js
+++ static/js/zhuiju365.js
@@ -26,23 +26,38 @@ $(function(){
             });
         }
     });
-    function dialogInfo(msg) {
-        $('#dialogInfo').remove();
-        var html = '';
-        html = '<div class="modal fade" id="dialogInfo" tabindex="-1" role="dialog" aria-labelledby="dialogInfoTitle">';
-        html += '<div class="modal-dialog" role="document">';
-        html += '<div class="modal-content">';
-        html += '<div class="modal-header">';
-        html += '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
-        html += '<h4 class="modal-title" id="dialogInfoTitle">zhuiju365提示</h4>';
-        html += ' </div>';
-        html += '<div class="modal-body">';
-        html += '<p>'+msg+'</p>';
-        html += '</div>';
-        html += '</div>';
-        html += '</div>';
-        html += '</div>';
-        $('body').append(html);
-        $('#dialogInfo').modal('show')  
-    }
 })
+
+function dialogInfo(msg) {
+    $('#dialogInfo').remove();
+    var html = '';
+    html = '<div class="modal fade" id="dialogInfo" tabindex="-1" role="dialog" aria-labelledby="dialogInfoTitle">';
+    html += '<div class="modal-dialog" role="document">';
+    html += '<div class="modal-content">';
+    html += '<div class="modal-header">';
+    html += '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+    html += '<h4 class="modal-title" id="dialogInfoTitle">zhuiju365提示</h4>';
+    html += ' </div>';
+    html += '<div class="modal-body">';
+    html += '<p>'+msg+'</p>';
+    html += '</div>';
+    html += '</div>';
+    html += '</div>';
+    html += '</div>';
+    $('body').append(html);
+    $('#dialogInfo').modal('show')  
+}
+
+function is_mobile() { 
+    var regex_match = /(nokia|iphone|android|motorola|^mot-|softbank|foma|docomo|kddi|up.browser|up.link|htc|dopod|blazer|netfront|helio|hosin|huawei|novarra|CoolPad|webos|techfaith|palmsource|blackberry|alcatel|amoi|ktouch|nexian|samsung|^sam-|s[cg]h|^lge|ericsson|philips|sagem|wellcom|bunjalloo|maui|symbian|smartphone|midp|wap|phone|windows ce|iemobile|^spice|^bird|^zte-|longcos|pantech|gionee|^sie-|portalmmm|jigs browser|hiptop|^benq|haier|^lct|operas*mobi|opera*mini|320x320|240x320|176x220)/i; 
+    var u = navigator.userAgent; 
+    if (null == u) { 
+        return true; 
+    } 
+    var result = regex_match.exec(u);
+    if (null == result) { 
+        return false 
+    } else { 
+        return true 
+    } 
+}
